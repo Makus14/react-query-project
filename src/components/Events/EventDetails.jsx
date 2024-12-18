67cc37d7dc229cd45dfbf67d4b1d7d5dd1be9608
Makus14 <mesha.maksim14@gmail.com>
2024-12-18 17:55:40 +0300

    display and delete event

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,8 +1,39 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams, useNavigate } from "react-router-dom";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
-import Header from '../Header.jsx';
+import Header from "../Header.jsx";
+import { fetchEvent, deleteEvent } from "../../util/http.js";
 
 export default function EventDetails() {
+  const params = useParams();
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["event", params.id],
+    queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
+  });
+
+  const { mutate, isPending: isDeleting } = useMutation({
+    mutationFn: () => deleteEvent({ id: params.id }),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["events"]);
+      navigate("/events");
+    },
+    onError: (err) => {
+      alert(`Error deleting event: ${err.message}`);
+    },
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
+
+  if (!data) return <p>No data found</p>;
+
+  function handleDeleteEvent() {
+    mutate();
+  }
+
   return (
     <>
       <Outlet />
@@ -13,20 +44,22 @@ export default function EventDetails() {
       </Header>
       <article id="event-details">
         <header>
-          <h1>EVENT TITLE</h1>
+          <h1>{data.title}</h1>
           <nav>
-            <button>Delete</button>
+            <button onClick={handleDeleteEvent} disabled={isDeleting}>
+              {isDeleting ? "Deleting..." : "Delete"}
+            </button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
         <div id="event-details-content">
-          <img src="" alt="" />
+          <img src={`http://localhost:3000/${data.image}`} alt="image" />
           <div id="event-details-info">
             <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>{data.date}</time>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
+            <p id="event-details-description">{data.description}</p>
           </div>
         </div>
       </article>
